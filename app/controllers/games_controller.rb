7578e56154252276181e38354ee2d2cf75ffcc0c
aangelinaa <angelina.wedemeyer@gmail.com>
2016-07-25 09:05:45 -0400

    use group location when event location is private to orient map

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,15 +3,29 @@ class GamesController < ApplicationController
   def index
     @games = Game.where('time > ?', (DateTime.now - 1.hour).strftime('%Q')).where('time < ?', (DateTime.now.beginning_of_day + 1.day).strftime('%Q')).order(:time)
     @hash = Gmaps4rails.build_markers(@games) do |game, marker|
-      marker.lat game.loc_lat
-      marker.lng game.loc_lon
-      marker.infowindow game.am_pm(game.time) + ' - ' + game.group_name
-      marker.picture({
-        "url" => "http://i.imgur.com/2j6UX2Q.png?3",
-        "width" => 27,
-        "height" => 27
-        })
+      if (game.loc_lat.nil? || game.loc_lon.nil?)
+        marker.lat game.group_lat
+        marker.lng game.group_lon
+        marker.picture({
+          "url" => "http://imgur.com/a/78MEL",
+          "width" => 27,
+          "height" => 27
+          })
+      else
+        marker.lat game.loc_lat
+        marker.lng game.loc_lon
+        marker.infowindow game.am_pm(game.time) + ' - ' + game.group_name
+        marker.picture({
+          "url" => "http://i.imgur.com/2j6UX2Q.png?3",
+          "width" => 27,
+          "height" => 27
+          })
+      end
     end
   end
 
 end
+
+
+
+#64
